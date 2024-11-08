f5d03bd27f93717849cc27f25aa3bfff5be91249
viuvanegra1411 <00001130208072sp@al.educacao.sp.gov.br>
2024-11-08 16:58:51 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'materia',
+    'O que é geografia?',
+    ' e a materia que estuda mapas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'e brasilia'
+)
+
+criaCartao(
+    'historia',
+    'quem descobriu o brasil?',
+    'pedro alves cabral'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ola em Inglês?',
+    'Oi em ingles é hello(RAI)'
+)
